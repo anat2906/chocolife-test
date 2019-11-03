e3da692d9f836bd10ec84219477adeb68f82decd
unknown <t2906a@gmail.com>
2019-11-03 10:09:47 +0600

    change views to methods

--- src/mobx/stores/ContactsStore.js
+++ src/mobx/stores/ContactsStore.js
@@ -1,31 +1,67 @@
+import axios from "axios";
 import { types } from "mobx-state-tree";
+import BASE_URL from "../../config/urls";
 import { ContactModel } from "../models/ContactModel";
 
 export const ContactsStore = types
-  .model({ contacts: types.array(ContactModel) })
+  .model({
+    contacts: types.optional(types.array(ContactModel), []),
+    isReverse: types.optional(types.boolean, false),
+    isFavorite: types.optional(types.boolean, false),
+    queryString: types.optional(types.string, "")
+  })
   .actions(self => ({
+    getContacts() {
+      console.log("getContacts");
+      if (self.contacts && self.contacts.length) return;
+      axios
+        .get(BASE_URL)
+        .then(response => {
+          self.setContacts(response.data.data);
+          return;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
     setContacts(contacts) {
       self.contacts = contacts;
+    },
+    setReverse() {
+      self.isReverse = true;
+    },
+    setAlphabet() {
+      self.isReverse = false;
+    },
+    toggleFavorite() {
+      self.isFavorite = !self.isFavorite;
+    },
+    setQuerySting(q) {
+      self.queryString = q;
     }
   }))
   .views(self => ({
-    get alphabetOrder() {
-      return self.contacts.sort(function(a, b) {
+    normaliziedData() {
+      let results = self.contacts.sort(function(a, b) {
         if (a.firstName < b.firstName) return -1;
         else if (a.firstName > b.firstName) return 1;
         return 0;
       });
-    },
-    reverseAlphaberOrder() {
-      return self.contacts
-        .sort(function(a, b) {
-          if (a.firstName < b.firstName) return -1;
-          else if (a.firstName > b.firstName) return 1;
-          return 0;
-        })
-        .reverse();
-    },
-    onlyFavorite() {
-      return self.contacts.filter(c => c.isFavorite);
+      if (self.isReverse) {
+        results = results.reverse();
+      }
+      if (self.isFavorite) {
+        results = results.filter(r => r.isFavorite);
+      }
+      if (self.queryString) {
+        results = results.filter(r => {
+          const str = self.queryString.toLowerCase();
+          const fn = r.firstName.toLowerCase();
+          const ln = r.lastName.toLowerCase();
+          return fn.startsWith(str) || ln.startsWith(str);
+        });
+      }
+
+      return results;
     }
   }));
