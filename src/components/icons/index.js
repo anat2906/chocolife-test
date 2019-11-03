28f30c6abb1c2921bcaeb8470d080dbefd918f39
unknown <t2906a@gmail.com>
2019-11-03 10:08:32 +0600

    add icon components

---
+++ src/components/icons/index.js
@@ -0,0 +1,19 @@
+import { FavouriteIcon } from "./FavouriteIcon";
+import { AlphabetSortIcon } from "./AlphabetSortIcon";
+import { ReverseSortIcon } from "./ReverseSortIcon";
+import { EmailIcon } from "./EmailIcon";
+import { LocationIcon } from "./LocationIcon";
+import { WebsiteIcon } from "./WebsiteIcon";
+import { PhoneIcon } from "./PhoneIcon";
+import { LogoIcon } from "./LogoIcon";
+
+export {
+  FavouriteIcon,
+  AlphabetSortIcon,
+  ReverseSortIcon,
+  EmailIcon,
+  LocationIcon,
+  WebsiteIcon,
+  PhoneIcon,
+  LogoIcon
+};
